878e2fce35bf438339b33442626a5db9c6589b10
sanju9977 <sanjunaik9977@gmail.com>
2023-04-20 14:01:48 +0530

    amazonMQ

--- terraform-eks/Terraform/Modules/amazonMQ.tf
+++ terraform-eks/Terraform/Modules/amazonMQ.tf
@@ -1,7 +1,7 @@
 resource "aws_mq_broker" "mq_broker" {
-  name     = "my-mq-broker"
-  engine_type = "ActiveMQ"
-  engine_version = "5.15.14"
+  name            = "my-mq-broker"
+  engine_type     = "ActiveMQ"
+  engine_version  = "5.15.14"
   deployment_mode = "SINGLE_INSTANCE"
 
   users = [
@@ -10,3 +10,4 @@ resource "aws_mq_broker" "mq_broker" {
       password = "admin"
     }
   ]
+}
